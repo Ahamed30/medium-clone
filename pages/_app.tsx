aff2c346e57c61d8f104db308e7077801a37730e
Dhanish Ahamed <dhanish.ahamed@bounteous.com>
2022-09-26 15:49:18 +0530

    solved hydration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return typeof window === 'undefined' ? <></> : <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp
